e64070fa4317cb62b0f858bd67e1f9a6e4d8011b
Alexandre Jacquot <jacquot.alexandre97@orange.fr>
2016-12-20 03:42:42 +0100

    ajout des controleurs Home, Images et Albums avec leurs vues (à completer)

---
+++ ProjetWeb/Controllers/AlbumsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetWeb.Models;
+
+namespace ProjetWeb.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private Classique_WebEntities db = new Classique_WebEntities();
+
+        public ActionResult Index()
+        {
+            var album = db.Album.Include(a => a.Editeur).Include(a => a.Genre);
+            return View(album.ToList());
+        }
+        
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Album.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+    }
+}
---
+++ ProjetWeb/Controllers/ImagesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using ProjetWeb.Models;
+using System.Net;
+
+namespace ProjetWeb.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private Classique_WebEntities db = new Classique_WebEntities();
+
+        public ActionResult Index()
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        public ActionResult PochetteAlbum(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Album.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return base.File(album.Pochette, "image/jpg");
+        }
+    }
+}
