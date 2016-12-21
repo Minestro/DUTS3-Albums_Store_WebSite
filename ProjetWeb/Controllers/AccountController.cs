f68c216267e2a81fce6dc2bd06b2665a43a6fd2c
Alexandre Jacquot <jacquot.alexandre97@orange.fr>
2016-12-21 19:55:01 +0100

    ajout de la connexion et de l'inscription utilisateur

---
+++ ProjetWeb/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using ProjetWeb.Models;
+using ProjetWeb.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ProjetWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private Classique_WebEntities db = new Classique_WebEntities();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            LoginViewModel model = new LoginViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel user, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                Abonné userLoged = user.login(); 
+                if (userLoged == null)
+                {
+                    ModelState.AddModelError("LoginOuEmail", "Login et/ou mot de passe incorrect(s)");
+                } else
+                {
+                    FormsAuthentication.SetAuthCookie(userLoged.Code_Abonné.ToString(), false);
+                    if (!string.IsNullOrWhiteSpace(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                Abonné userRegistered = user.register();
+                FormsAuthentication.SetAuthCookie(userRegistered.Code_Abonné.ToString(), false);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ProjetWeb/ViewModels/UtilisateurViewModels.cs
@@ -0,0 +1,98 @@
+﻿using ProjetWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ProjetWeb.ViewModels
+{
+    static class Encode
+    {
+        public static string EncodeMD5(string password)
+        {
+            string salt = "MotDePasse" + password + "ASP.NET MVC";
+            return BitConverter.ToString(new MD5CryptoServiceProvider().ComputeHash(ASCIIEncoding.Default.GetBytes(salt)));
+        }
+    }
+
+    public static class User
+    {
+        public static Abonné getUserByID(int id)
+        {
+            Classique_WebEntities db = new Classique_WebEntities();
+            return db.Abonné.FirstOrDefault(u => u.Code_Abonné == id);
+        }
+    }
+
+    public class RegisterViewModel
+    {
+        public int Code_Abonné { get; set; }
+
+        [Required]
+        [Display(Name = "Nom")]
+        public string Nom_Abonné { get; set; }
+
+        [Display(Name = "Prénom")]
+        public string Prénom_Abonné { get; set; }
+
+        [Required]
+        [Display(Name = "Pseudo")]
+        public string Login { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "La chaîne {0} doit comporter au moins {2} caractères.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe ")]
+        [Compare("Password", ErrorMessage = "Le mot de passe et le mot de passe de confirmation ne correspondent pas.")]
+        public string PasswordConfirmation { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public Abonné register()
+        {
+            string passwordEncoded = Encode.EncodeMD5(Password);
+            Classique_WebEntities db = new Classique_WebEntities();
+            Abonné abonné = new Abonné { Nom_Abonné = this.Nom_Abonné, Prénom_Abonné = this.Prénom_Abonné, Login = this.Login, Password = passwordEncoded, Email = this.Email, Credit = 0 };
+            db.Abonné.Add(abonné);
+            db.SaveChanges();
+            return abonné;
+        }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Pseudo ou Email")]
+        public string LoginOuEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        public Abonné login()
+        {
+            string passwordEncoded = Encode.EncodeMD5(Password);
+            Classique_WebEntities db = new Classique_WebEntities();
+            Abonné user = db.Abonné.FirstOrDefault(u => u.Login == LoginOuEmail && u.Password == passwordEncoded);
+            if (user == null)
+            {
+                return db.Abonné.FirstOrDefault(u => u.Email == LoginOuEmail && u.Password == passwordEncoded);
+            }
+            else
+            {
+                return user;
+            }
+        }
+    }
+}
