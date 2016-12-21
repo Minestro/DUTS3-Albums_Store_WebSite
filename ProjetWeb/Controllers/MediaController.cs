600eadfab016243b927a41b43299866b45adf4c7
Alexandre Jacquot <jacquot.alexandre97@orange.fr>
2016-12-21 20:46:34 +0100

    ajout des extrait de musique

--- ProjetWeb/Controllers/MediaController.cs
+++ ProjetWeb/Controllers/MediaController.cs
@@ -9,7 +9,7 @@ using System.Net;
 
 namespace ProjetWeb.Controllers
 {
-    public class ImagesController : Controller
+    public class MediaController : Controller
     {
         private Classique_WebEntities db = new Classique_WebEntities();
 
@@ -30,6 +30,20 @@ namespace ProjetWeb.Controllers
                 return HttpNotFound();
             }
             return base.File(album.Pochette, "image/jpg");
+        }
+
+        public ActionResult ExtraitMusique(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enregistrement enregistrement = db.Enregistrement.Find(id);
+            if (enregistrement == null)
+            {
+                return HttpNotFound();
+            }
+            return base.File(enregistrement.Extrait, "audio/mpeg");
         }
     }
 }
