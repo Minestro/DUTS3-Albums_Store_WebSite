295c9e2b0e12d651bae93eaa5eece75010c040bb
Florent Mariotti <florent.mariotti@etu.u-bordeaux.fr>
2017-01-11 22:55:17 +0100

    Ajout de la page Musicien + ajout de la liste des Musicien dans la page Album (doit être changé en Compositeur)

--- ProjetWeb/ViewModels/AlbumViewModels.cs
+++ ProjetWeb/ViewModels/AlbumViewModels.cs
@@ -12,8 +12,11 @@ namespace ProjetWeb.ViewModels
         public Album Album {get; set;}
         public string Description {get; set;}
         public string Prix { get; set; }
-        public List<Enregistrement> Enregistrements { get; set; } 
-         
+        public List<Enregistrement> Enregistrements { get; set; }
+        public List<Enregistrement> EnregistrementsTest { get; set; }
+        public List<Musicien> Musiciens { get; set; }
+        public List<Composition> Compositeurs { get; set; }
+
         public AlbumViewModels (Album album)
         {
             Album = album;
@@ -24,6 +27,13 @@ namespace ProjetWeb.ViewModels
                     .SelectMany(c => db.Disque.Where(o => o.Code_Album == c.Code_Album))
                     .SelectMany(c => db.Composition_Disque.Where(o => o.Code_Disque == c.Code_Disque))
                     .SelectMany(c => db.Enregistrement.Where(o => o.Code_Morceau == c.Code_Morceau)).ToList();
+
+                Musiciens = db.Album.Where(m => m.Code_Album == album.Code_Album)
+                    .SelectMany(m => db.Genre.Where(p => p.Code_Genre == m.Code_Genre))
+                    .SelectMany(m => db.Musicien.Where(p => p.Code_Genre == m.Code_Genre)).ToList();
+                    //.SelectMany(m => db.Composer.Where(p => p.Code_Musicien == m.Code_Musicien)).ToList();
+
+                
             }
 
         }
